0fb56f817ebd9e025f2c6928f3c01e972f433f56
Collin Tharp <catharp@gmail.com>
2016-11-29 12:31:12 -0800

    add inputs for logging progress

--- client/app/goals/controller.js
+++ client/app/goals/controller.js
@@ -1,19 +1,23 @@
 angular.module('goaltender.controller', [])
 
 .controller('GoalsController', function($scope, $location, Goals, moment) {
-  $scope.newGoal = {};
-  $scope.goals = [];
-
-  console.log(moment().fromNow());
-
-  var setGoals = function () {
+  function setGoals() {
     Goals.get()
       .then(function (goals) {
         $scope.goals = goals;
       });
   };
 
-  $scope.addGoal = function () {
+  setGoals();
+
+  $scope.addProgress = function(goal) {
+    goal.progress.push({
+      date: Date.now(),
+      log: goal.newProgress
+    });
+  };
+
+  $scope.addGoal = function() {
     $scope.newGoal.deadline = Date.now() + $scope.newGoal.deadline * 24 * 60 * 60 * 1000;
     Goals.add($scope.newGoal)
       .then(function() {
@@ -21,5 +25,4 @@ angular.module('goaltender.controller', [])
       });
   };
 
-  setGoals();
 });
