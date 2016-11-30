7927f02957b305cc7460de807c3819811ba0df98
Collin Tharp <catharp@gmail.com>
2016-11-29 16:21:42 -0800

    implement multi-stage goal building

--- client/app/goals/controller.js
+++ client/app/goals/controller.js
@@ -1,22 +1,26 @@
 angular.module('goaltender.controller', [])
 
-.controller('GoalsController', function($scope, $location, Goals, moment) {
+.controller('GoalsController', function ($scope, $location, Goals, moment) {
   function setGoals() {
     Goals.get()
       .then(function (goals) {
         $scope.goals = goals;
       });
   };
-
   setGoals();
 
-  $scope.addGoal = function() {
+  $scope.building = false;
+  $scope.buildGoal = function () {
+    $scope.building = true;
+  }
+
+  $scope.addGoal = function () {
     $scope.newGoal.deadline = Date.now() + $scope.newGoal.deadline * 24 * 60 * 60 * 1000;
     Goals.add($scope.newGoal)
       .then(() => $location.path('/'));
   };
 
-  $scope.addProgress = function(goal) {
+  $scope.addProgress = function (goal) {
     goal.progress.push({
       date: Date.now(),
       log: goal.newProgress
@@ -25,7 +29,7 @@ angular.module('goaltender.controller', [])
     delete goal.newProgress;
   };
 
-  $scope.abandonGoal = function(goal) {
+  $scope.abandonGoal = function (goal) {
     Goals.abandon(goal)
       .then(() => $location.path('/'));
   };
